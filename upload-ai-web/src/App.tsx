f6a89d53e7d32bc7ce345e9b9e92c1db93daa0f5
Gean Bertani <geanbertani@gmail.com>
2023-09-16 11:26:16 -0400

    :sparkles: feat(app): configuring the select prompt and temperature slider components

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -6,8 +6,17 @@ import { Label } from "./components/ui/label";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
 import { VideoInputForm } from "./components/video-input-form";
+import { PromptSelect } from "./components/prompt-select";
+import { useState } from "react";
 
 export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+
+
+  function handlePromptSelected(template: string) {
+    console.log(template)
+  }
+
   return (
     <div className="min-h-screen flex flex-col">
       <div className="px-6 py-3 flex items-center justify-between border-b">
@@ -51,15 +60,7 @@ export function App() {
           <form className="space-y-6">
             <div className="space-y-2">
               <Label>Prompt</Label>
-              <Select>
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione um prompt..." />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="title">Título do YouTube</SelectItem>
-                  <SelectItem value="description">Descrição do YouTube</SelectItem>
-                </SelectContent>
-              </Select>
+              <PromptSelect onPromptSelected={handlePromptSelected} />
             </div>
 
             <div className="space-y-2">
@@ -85,6 +86,8 @@ export function App() {
                 min={0}
                 max={1}
                 step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
                 className="cursor-pointer"
               />
               <span className="block text-xs text-muted-foreground italic leading-relaxed">
