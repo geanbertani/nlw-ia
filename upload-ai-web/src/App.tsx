0439acd1b4090b09f0ebce09408a81b6b6af5525
Gean Bertani <geanbertani@gmail.com>
2023-09-16 11:33:31 -0400

    :sparkles: feat(app): videoId capture by the VideoInputForm component

--- upload-ai-web/src/App.tsx
+++ upload-ai-web/src/App.tsx
@@ -11,11 +11,13 @@ import { useState } from "react";
 
 export function App() {
   const [temperature, setTemperature] = useState(0.5)
-
+  const [videoId, setVideoId] = useState<string | null>(null)
 
   function handlePromptSelected(template: string) {
     console.log(template)
   }
+
+
 
   return (
     <div className="min-h-screen flex flex-col">
@@ -53,7 +55,7 @@ export function App() {
         </div>
 
         <aside className="w-80 space-y-6">
-          <VideoInputForm />
+          <VideoInputForm onVideoUploaded={setVideoId} />
 
           <Separator />
 
